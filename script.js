cad32d23624dffaed015296d3676cf1084391b1f
NineChips <115571967+NineChips@users.noreply.github.com>
2024-12-02 18:59:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const countdownDate = new Date("December 20, 2023 00:00:00").getTime();
+
+const countdownDate = new Date("December 20, 2024 00:00:00").getTime();
 
 const countdownFunction = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownDate - now;
 
+    
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -13,7 +15,6 @@ const countdownFunction = setInterval(() => {
     document.getElementById("hours").innerText = String(hours).padStart(2, '0');
     document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
     document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
-
     if (distance < 0) {
         clearInterval(countdownFunction);
         document.querySelector('.countdown').innerHTML = "<h1>Countdown Finished!</h1>";
