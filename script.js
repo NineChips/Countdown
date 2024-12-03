91afd578ab590547ce49971d201f4303e76d0011
NineChips <115571967+NineChips@users.noreply.github.com>
2024-12-02 19:00:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+var countDownDate = new Date("Dec 20, 2024 13:30:00").getTime();
 
-const countdownDate = new Date("December 20, 2024 00:00:00").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-const countdownFunction = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
+  // Get today's date and time
+  var now = new Date().getTime();
 
-    
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-    document.getElementById("days").innerText = String(days).padStart(2, '0');
-    document.getElementById("hours").innerText = String(hours).padStart(2, '0');
-    document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
-    document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
-    if (distance < 0) {
-        clearInterval(countdownFunction);
-        document.querySelector('.countdown').innerHTML = "<h1>Countdown Finished!</h1>";
-    }
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
